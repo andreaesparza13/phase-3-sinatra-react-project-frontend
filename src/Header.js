efee5099ffce2c83d85f17c9b890661fa79abd19
Jared <mumawjared@gmail.com>
2022-08-23 12:07:02 -0700

    tailwind and header

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="mr-auto w-32 h-16 bg-stone-500 text-center pt-4 rounded-br-md">
+      <span className="font-bold text-lg">Art exhibits</span>
+    </div>
+  );
+};
+
+export default Header;
