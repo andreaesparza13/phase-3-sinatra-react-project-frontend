9dc40fe93db0d9010af8a7c0ab9e6511c763a550
Loren Wilks <Loren.wilks@gmail.com>
2022-08-25 15:40:22 -0600

    made a form for new art

--- src/components/ArtForm.js
+++ src/components/ArtForm.js
@@ -2,7 +2,67 @@ import React, { useState } from 'react';
 
 const ArtForm = () => {
   return (
-    <div>NEW</div>
+    <form className="w-full max-w-xl items-center">
+      <h1 className="font-medium leading-tight text-5xl mt-0 mb-2 text-black-600">New Art</h1>
+      <div className="md:flex md:items-center mb-6">
+        <div className="md:w-1/3">
+          <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+            Title
+          </label>
+        </div>
+        <div className="md:w-2/3">
+          <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-first-name" type="text" />
+        </div>
+      </div>
+      <div className="md:flex md:items-center mb-6">
+        <div className="md:w-1/3">
+          <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+            Description
+          </label>
+        </div>
+        <div className="md:w-2/3">
+          <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-last-name" type="text" />
+        </div>
+      </div>
+      <div className="md:flex md:items-center mb-6">
+        <div className="md:w-1/3">
+          <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+            Image URL
+          </label>
+        </div>
+        <div className="md:w-2/3">
+          <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-location" type="text" />
+        </div>
+      </div>
+      <div className="md:flex md:items-center mb-6">
+        <div className="md:w-1/3">
+          <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+            Artist
+          </label>
+        </div>
+        <div className="md:w-2/3">
+          <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-age" type="text" />
+        </div>
+      </div>
+      <div className="md:flex md:items-center mb-6">
+        <div className="md:w-1/3">
+          <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+            Exhibit
+          </label>
+        </div>
+        <div className="md:w-2/3">
+          <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-age" type="text" />
+        </div>
+      </div>
+      <div className="md:flex md:items-center">
+        <div className="md:w-1/3"></div>
+        <div className="md:w-2/3">
+          <button className="shadow bg-blue-500 hover:bg-blue-600 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+            Submit
+          </button>
+        </div>
+      </div>
+  </form>
   )
 }
 
