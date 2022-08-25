0b5cb4095d78af86b5abb1e7789e46440792c5a1
professorjrod <73684895+professorjrod@users.noreply.github.com>
2022-08-25 14:08:55 -0700

    added name
    added name

--- src/components/Art.js
+++ src/components/Art.js
@@ -1,4 +1,4 @@
-const Art = ({ title, medium, description, image_url }) => {
+const Art = ({ title, medium, description, image_url, artist }) => {
 
     return (
         <div className="flex justify-center">
@@ -6,7 +6,7 @@ const Art = ({ title, medium, description, image_url }) => {
                 <img className="rounded-t-lg" src={image_url} alt={title}/>
                 <div className="p-6">
                 <h3 className="text-gray-900 text-2xl font- mb-2">{title}</h3>
-                <h5 className="text-gray-800 text-lg font-medium mb-2">[ADD ARTIST NAME HERE SOMEHOW]</h5>
+                <h5 className="text-gray-800 text-lg font-medium mb-2">{artist}</h5>
                 <h5 className="text-gray-800 text-lg font-medium mb-2">{medium}</h5>
                 <p className="text-gray-700 text-base mb-4">
                     {description}
@@ -20,17 +20,3 @@ const Art = ({ title, medium, description, image_url }) => {
 
 export default Art;
 
-
-
-{/* <div className="p-2 m-auto w-64 h-xl bg-blue-500 border-2 border-black mb-6">
-<ul>
-    <span>[Click to view Artist]</span>
-    <li>{title}</li>
-    <br/>
-    <li>{medium}</li>
-    <br/>
-    <li>{description}</li>
-    <br/>
-</ul>
-<img class="rounded-t-lg" src={image_url} alt={title}/>
-</div> */}
--- src/components/ArtList.js
+++ src/components/ArtList.js
@@ -1,19 +1,23 @@
 import React from 'react'
 import Art from './Art'
 
-export const ArtList = ({arts}) => {
+export const ArtList = ({arts, artists}) => {
   if(!arts || arts.length === 0) {
     return <div>Nothing to display...</div>
   }
-  // console.log(arts)
+
   return (
     <div className="flex flex-row">
-      {arts.map(art => (
+      {arts.map(art => 
+        (
          <Art
             title={art.title}
             medium={art.medium}
             description={art.description}
             image_url={art.image_url}
+            artist={
+              artists.find(artist => artist.id === art.artist_id).first_name
+            }
             key={art.id}
          />)
       )}
